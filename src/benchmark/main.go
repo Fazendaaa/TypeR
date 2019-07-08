4bdff984f9b3ed20a2d904885b002871c9a2e88b
Fazendaaa <lucas.carotta@outlook.com>
2019-07-08 17:39:14 -0300

    Adding benchmark module.

---
+++ src/benchmark/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"../compiler"
+	"../evaluator"
+	"../lexer"
+	"../object"
+	"../parser"
+	"../virtualmachine"
+)
+
+var engine = flag.String("engine", "virtualmachine", "user 'virtualmachine' or 'evaluator'")
+
+var input = `
+let fibonacci <-function(x) {
+	if (0 == x) {
+		0
+	} else {
+		if (1 == x) {
+			1
+		} else {
+			fibonacci(x - 1) + fibonacci(x - 2)
+		}
+	}
+}
+
+fibonacci(35)
+`
+
+func main() {
+	flag.Parse()
+
+	var duration time.Duration
+	var result object.Object
+
+	l := lexer.InitializeLexer(input)
+	p := parser.InitializeParser(l)
+	program := p.ParseProgram()
+
+	if "virtualmachine" == *engine {
+		comp := compiler.InitializeCompiler()
+		err := comp.Compile(program)
+
+		if nil != err {
+			fmt.Printf("compiler error: %s", err)
+
+			return
+		}
+
+		machine := virtualmachine.InitializeVirtualMachine(comp.Bytecode())
+
+		start := time.Now()
+
+		err = machine.Run()
+
+		if nil != err {
+			fmt.Printf("Virtual Machine error: %s", err)
+
+			return
+		}
+
+		duration = time.Since(start)
+
+		result = machine.LastPoppedStackElement()
+	} else {
+		env := object.InitializeEnvironment()
+		start := time.Now()
+		result = evaluator.Eval(program, env)
+		duration = time.Since(start)
+	}
+
+	fmt.Printf("engine=%s, result=%s, duration=%s\n", *engine, result.Inspect(), duration)
+}
